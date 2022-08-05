56ce13105b5a8acadbdda38cb8840e14fa83e2d0
Grochowsky <grzesiugg@gmail.com>
2022-08-05 20:01:39 +0200

    lerning

--- index.js
+++ index.js
@@ -3,25 +3,31 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const { ppid } = require('process');
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname + '/views'))
+
+app.set('view engine', 'ejs') // musimy powiedziec ze bedziemy korzystac z ejs
+app.set('views', path.join(__dirname + '/views')) //tu musimy wskazac scezke pliku z widokami
 
 
 
 app.get('/', (req,res) => {
-    res.render('home')
+    res.render('home', {name1} )
 })
 
+const name1 = 'sasdasd';
 
 app.get('/firmy/:name', (req,res) => {
     const { name } = req.params;
-    const compnies = [
+    const companies = [
         { slug: 'tworcastron', name: 'Tworc Stron.pl' },
         { slug: 'brukmode', name: 'Bruk Mode' }
-    ]
+    ];
    
     const company = companies.find(x => x.slug === name );
-    res.render('company', { name: company?.name })
+
+    res.render('company', { 
+        name: company?.name, 
+        companies,
+    })
 })
 
 
