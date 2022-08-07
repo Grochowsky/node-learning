d5fcec06e5dcec499e2ca2baf9509cc1fdcb9b7a
Grochowsky <grzesiugg@gmail.com>
2022-08-07 21:18:39 +0200

    lerning Schema

--- db-moongose.js
+++ db-moongose.js
@@ -1,19 +1,21 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema; // pobieraamy schema
 
 mongoose.connect('mongodb://localhost:27017/node-kurs')
 
-//w moongose tworymy model firmy
-
-const Company = mongoose.model('Company', {
+//model
+const companySchema = new Schema({
     slug: {
         type: String,
-        required: [true, 'Pole slug jest wymagane'],
-        minLength: [3, ' Minimalnaa liczba znaków w slug to 3'],
+        required: [true, 'Pole slug jest wymagane'], // pole slug jest wymagane
+        minLength: [3, ' Minimalnaa liczba znaków w slug to 3'], // minimalna liczba znakow
         valaidate: value => {
             if (value==="slug"){
                 throw new Err('Nazwa SLUG jest zakazana'); // customowa waalidacja. jesli dodasz slug name to wtedy wyrzuci blad
             }
-        }
+        },
+        trim: true,  // usuwa biale znaki
+        lowercase:true, // zmienia wszystko na male litery
     },
     name: {
         type: String,
@@ -22,16 +24,24 @@ const Company = mongoose.model('Company', {
     employesCount: {
         type: Number,
         min: 1,
+        default: 1,
     }
 })
+const Company = mongoose.model('Company', companySchema )
+
+
+// setter tworzenie - przypisujemy do schematu
+companySchema.path('slug').set((value) => value.toLowerCase())
+companySchema.path('name').set((value) => value.toLowerCase())
+
 
 async function main(){
     //const companies = await Company.find({});
     //console.log(companies);
 
     const company  = new Company ({
-        name: '232',
-        slug: "sss",
+        name: '    Jede  ',
+        slug: "  Ss  ss",
         employesCount: 2,
     });
     try {
